7df8ba1bcdc7e34510552fec4e6a0b013ae21212
WardPearce <wardpearce@pm.me>
2024-11-01 14:28:33 +1300

    Added console support

--- src/lib/components/server/Console.svelte
+++ src/lib/components/server/Console.svelte
@@ -1,5 +1,35 @@
+<script lang="ts">
+	import { puffer } from '$lib/const';
+	import { createEventDispatcher, onMount, tick } from 'svelte';
+
+	let props: { serverId: string } = $props();
+
+	const dispatch = createEventDispatcher();
+
+	let serverLogs: string = $state('');
+	onMount(async () => {
+		serverLogs = atob(`${(await puffer.default.getApiServersConsole(props.serverId, 0)).logs}`);
+		scrollIntoView();
+	});
+
+	async function scrollIntoView() {
+		await tick();
+		const consoleElement = document.getElementById('console');
+		if (consoleElement) consoleElement.scrollTop = consoleElement.scrollHeight;
+	}
+
+	function websocket(data: Record<string, any>) {
+		serverLogs += atob(data.logs);
+		scrollIntoView();
+	}
+
+	dispatch('websocket', websocket);
+</script>
+
 <article>
-	<article class="surface-dim"></article>
+	<article id="console" class="surface-dim large" style="overflow: scroll;white-space: pre-line;">
+		{serverLogs}
+	</article>
 	<nav>
 		<div class="field label border max">
 			<input type="text" />
--- src/routes/me/server/[slug]/+page.svelte
+++ src/routes/me/server/[slug]/+page.svelte
@@ -1,5 +1,40 @@
-<script>
+<script lang="ts">
 	import Console from '$lib/components/server/Console.svelte';
+	import { onDestroy, onMount } from 'svelte';
+
+	let { data }: { data: { serverId: string } } = $props();
+
+	let consoleFunc: Function;
+
+	let websocket: WebSocket;
+	onMount(async () => {
+		const pufferUrl = new URL(import.meta.env.VITE_DEFAULT_PUFFER_PANEL);
+
+		websocket = new WebSocket(
+			`${pufferUrl.protocol === 'http:' ? 'ws' : 'wss'}://${pufferUrl.host}/api/servers/${data.serverId}/socket`
+		);
+
+		websocket.onmessage = (event: MessageEvent) => {
+			let message: Record<string, any> | undefined = undefined;
+			try {
+				message = JSON.parse(event.data);
+			} catch (error) {}
+
+			if (message) {
+				if (message.type === 'console') {
+					consoleFunc(message.data);
+				}
+			}
+		};
+	});
+
+	onDestroy(() => {
+		websocket.close();
+	});
+
+	function handleWebhookReady(event: { detail: Function }) {
+		consoleFunc = event.detail;
+	}
 </script>
 
 <div>
@@ -37,4 +72,4 @@
 
 <div class="small-space"></div>
 
-<Console />
+<Console serverId={data.serverId} on:websocket={handleWebhookReady} />
